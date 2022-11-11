99b0da5dbe0f9752df66f775471411b6525a9a37
Muhammad Rafii <myasykurrafii2@gmail.com>
2022-11-11 21:12:11 +0700

    fix(API): :bug: fixing bug for hosting

--- hypeandplay/hypeandplay/settings.py
+++ hypeandplay/hypeandplay/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-^4=d9kr(vap4b$2-3jn#+49r4^&m%f9hdygillsdwf3i_%5)w=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["hypeandplayserver.com"]
 
 
 # Application definition
